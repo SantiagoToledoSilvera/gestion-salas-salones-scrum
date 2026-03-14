a70dd4e61acfc72be9f213f9a06bc06b1f7fc847
juanpabloquuu7-del <juanpabloquuu7@gmail.com>
2026-03-14 14:21:57 -0500

    refactor(interfaz): actualizar mostrar_salas para usar Treeview de Tkinter

--- Interfaz.py
+++ Interfaz.py
@@ -1,7 +1,13 @@
-def mostrar_salas(lista_salas):
+import tkinter as tk
+from tkinter import messagebox
+from tkinter import ttk
 
-    print("\n--- Lista de salas ---")
+
+def mostrar_salas(tree, lista_salas):
+
+    # Tarea: Mostrar lista de salas
+    for item in tree.get_children():
+        tree.delete(item)
 
     for s in lista_salas:
-
-        print("ID:", s.id_sala, "| Nombre:", s.nombre)
+        tree.insert("", "end", values=(s.id_sala, s.nombre))
